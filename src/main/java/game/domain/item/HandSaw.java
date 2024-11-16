03376d93c2c13ed09577e44c09e3dbd3b0df16b5
Kim5eongkwang <ksg6736@jbnu.ac.kr>
2024-11-17 02:04:50 +0900

    feat: 산탄총의 피해를 2만큼 주게 한다.

--- src/main/java/game/domain/item/HandSaw.java
+++ src/main/java/game/domain/item/HandSaw.java
@@ -1,10 +1,23 @@
 package game.domain.item;
 
+import game.domain.bullet.Bullets;
 import game.dto.ItemUsageRequestDto;
 
 public class HandSaw implements Item {
     @Override
-    public ItemUsageRequestDto useItem() {
-        return null;
+    public ItemUsageRequestDto useItem(ItemUsageRequestDto itemUsageRequestDto) {
+        Bullets newBullets = strengthenFirstBullet(itemUsageRequestDto);
+
+        return itemUsageRequestDto.changeGameData(itemUsageRequestDto
+                        .gameDataDto()
+                        .changeBullets(newBullets))
+                .reduceCasterItem(this);
+    }
+
+    private static Bullets strengthenFirstBullet(ItemUsageRequestDto itemUsageRequestDto) {
+        return itemUsageRequestDto
+                .gameDataDto()
+                .bullets()
+                .StrengthenFirstBullet();
     }
 }
