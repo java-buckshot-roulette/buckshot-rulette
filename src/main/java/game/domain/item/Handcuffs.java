b548e9a43e13d39be686a6af154f46906e0ad148
Kim5eongkwang <ksg6736@jbnu.ac.kr>
2024-11-17 03:48:58 +0900

    feat: 수갑, 상태가 다음 차례를 넘기도록 한다.

--- src/main/java/game/domain/item/Handcuffs.java
+++ src/main/java/game/domain/item/Handcuffs.java
@@ -1,10 +1,21 @@
 package game.domain.item;
 
+import game.domain.Role;
 import game.dto.ItemUsageRequestDto;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Handcuffs implements Item{
+public class Handcuffs implements Item {
     @Override
-    public ItemUsageRequestDto useItem() {
-        return null;
+    public ItemUsageRequestDto useItem(ItemUsageRequestDto itemUsageRequestDto) {
+        List<Role> newTurns = new ArrayList<>(itemUsageRequestDto.gameDataDto().turns());
+
+        Role role = newTurns.getFirst();
+        newTurns.addFirst(role);
+
+        return itemUsageRequestDto.changeGameData(itemUsageRequestDto
+                        .gameDataDto()
+                        .changeTurns(newTurns))
+                .reduceCasterItem(this);
     }
 }
