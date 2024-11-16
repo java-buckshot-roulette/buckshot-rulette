05c926051fd07554e4b787ed3a5e32289fe1e057
Kim5eongkwang <ksg6736@jbnu.ac.kr>
2024-11-17 03:04:22 +0900

    feat: 변환기, 약실에 장전된 탄환의 종류를 바꾼다.

--- src/main/java/game/domain/item/Inverter.java
+++ src/main/java/game/domain/item/Inverter.java
@@ -1,10 +1,17 @@
 package game.domain.item;
 
+import game.domain.bullet.Bullet;
+import game.domain.bullet.Bullets;
 import game.dto.ItemUsageRequestDto;
 
 public class Inverter implements Item{
     @Override
-    public ItemUsageRequestDto useItem() {
-        return null;
+    public ItemUsageRequestDto useItem(ItemUsageRequestDto itemUsageRequestDto) {
+        Bullets newBullets = itemUsageRequestDto.gameDataDto().bullets().invertFirstBullet();
+        return itemUsageRequestDto
+                .changeGameData(itemUsageRequestDto
+                        .gameDataDto()
+                        .changeBullets(newBullets))
+                .reduceCasterItem(this);
     }
 }
---
+++ src/test/java/game/domain/item/InverterTest.java
@@ -0,0 +1,70 @@
+package game.domain.item;
+
+import static game.domain.Role.CHALLENGER;
+import static game.domain.Role.DEALER;
+import static game.domain.bullet.BulletConfig.BLUE;
+import static game.domain.bullet.BulletConfig.RED;
+import static org.junit.jupiter.api.Assertions.*;
+
+import game.domain.LifeAndDeath;
+import game.domain.bullet.Bullet;
+import game.domain.bullet.Bullets;
+import game.domain.healthpoint.HealthPoint;
+import game.dto.GameStateDto;
+import game.dto.ItemUsageRequestDto;
+import game.dto.PlayerDataDto;
+import java.util.Collections;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class InverterTest {
+    @Test
+    void 빨간탄을_파란탄으로_교체() {
+        //given
+        PlayerDataDto caster = new PlayerDataDto(new HealthPoint(8), new Items(Collections.emptyList()),
+                LifeAndDeath.LIFE);
+
+        PlayerDataDto target = new PlayerDataDto(new HealthPoint(8), new Items(Collections.emptyList()),
+                LifeAndDeath.LIFE);
+
+        GameStateDto gameStateDto = new GameStateDto(new Bullets(List.of(RED)), List.of(CHALLENGER, DEALER));
+
+        ItemUsageRequestDto itemUsageRequestDto = new ItemUsageRequestDto(caster, target, gameStateDto);
+
+        //when
+        Item inverter = new Inverter();
+        ItemUsageRequestDto newItemRequest = inverter.useItem(itemUsageRequestDto);
+
+        //then
+        Assertions.assertThat(newItemRequest
+                        .gameDataDto()
+                        .bullets())
+                .isEqualTo(new Bullets(List.of(BLUE)));
+    }
+
+    @Test
+    void 푸른탄을_빨간탄으로_교체() {
+        //given
+        PlayerDataDto caster = new PlayerDataDto(new HealthPoint(8), new Items(Collections.emptyList()),
+                LifeAndDeath.LIFE);
+
+        PlayerDataDto target = new PlayerDataDto(new HealthPoint(8), new Items(Collections.emptyList()),
+                LifeAndDeath.LIFE);
+
+        GameStateDto gameStateDto = new GameStateDto(new Bullets(List.of(RED, BLUE)), List.of(CHALLENGER, DEALER));
+
+        ItemUsageRequestDto itemUsageRequestDto = new ItemUsageRequestDto(caster, target, gameStateDto);
+
+        //when
+        Item inverter = new Inverter();
+        ItemUsageRequestDto newItemRequest = inverter.useItem(itemUsageRequestDto);
+
+        //then
+        Assertions.assertThat(newItemRequest
+                        .gameDataDto()
+                        .bullets())
+                .isEqualTo(new Bullets(List.of(BLUE, BLUE)));
+    }
+
+}
