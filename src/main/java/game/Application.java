c4c633e140304da2e5bcb3c51073a2164c528490
kkh0920 <rudgns1535@gmail.com>
2024-11-19 19:11:26 +0900

    refactor: 불필요한 메소드 제거

--- src/main/java/game/Application.java
+++ src/main/java/game/Application.java
@@ -1,8 +1,6 @@
 package game;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 import game.config.StageConfig;
 import game.config.StageDependency;
--- src/main/java/game/service/player/AIPlayerService.java
+++ src/main/java/game/service/player/AIPlayerService.java
@@ -2,30 +2,25 @@ package game.service.player;
 
 import java.util.List;
 
+import static game.domain.bullet.BulletConfig.RED;
 import static game.domain.bullet.BulletConfig.BLUE;
 import static game.domain.item.ItemType.MAGNIFYING_GLASS;
 import static game.domain.item.ItemType.SHOT_GUN;
 
 import game.domain.Player;
 import game.domain.bullet.Bullet;
-import game.domain.bullet.BulletConfig;
 import game.config.StageDependency;
 import game.domain.item.Item;
-import game.domain.item.ItemType;
 import game.dto.GameStateDto;
 import game.dto.ItemUsageRequestDto;
 import game.dto.ItemUsageResponseDto;
 import game.dto.PlayerDataDto;
-import game.util.Convertor;
 import game.util.Timer;
 import game.view.output.OutputView;
 
 public class AIPlayerService implements PlayerService {
     private Player player;
     private OutputView outputView;
-
-    private int red;
-    private int blue;
 
     private Bullet nextBullet = null;
 
@@ -64,24 +59,26 @@ public class AIPlayerService implements PlayerService {
     private ItemUsageResponseDto processItemsUntilShotgun(PlayerDataDto rival, GameStateDto gameStateDto) {
         Item item;
         ItemUsageRequestDto newitemUsageRequestDto = makeTargetingRival(rival, gameStateDto);
-        while((item = readItem(newitemUsageRequestDto)).equals(SHOT_GUN.getInstance())) {
+        while(!(item = decideItem(newitemUsageRequestDto, gameStateDto)).equals(SHOT_GUN.getInstance())) {
             printUsingItem(item);
             if (item.equals(MAGNIFYING_GLASS.getInstance())) {
-                decideNextBullet(gameStateDto.bullets().CheckFirstBullet());
+                nextBullet = gameStateDto.bullets().CheckFirstBullet();
             }
             newitemUsageRequestDto = item.useItem(makeTargetingRival(rival, gameStateDto));
             return makeTargetingRival(newitemUsageRequestDto);
         }
+        printUsingItem(item);
         return useShotGun(item, rival, gameStateDto);
     }
 
-    private Item readItem(ItemUsageRequestDto itemUsageRequestDto) {
+    private Item decideItem(ItemUsageRequestDto itemUsageRequestDto, GameStateDto gameStateDto) {
         outputView.printPlayerState(itemUsageRequestDto.caster(), itemUsageRequestDto.target()); 
         Item item = SHOT_GUN.getInstance();
 
+        // TODO: gameStateDto.bullets()를 통해 가져온 (실탄 / 공포탄) 수를 통해 행동 방식 결정
         /*
-         * 1. 맥주: red, blue 조정 필요
-         * 2. 담배
+         * 1. 맥주
+         * 2. 담배: 체력이 감소된 상태라면, 무조건 사용
          * 3. 상한약
          * 4. 수갑
          * 5. 인버터
@@ -94,15 +91,26 @@ public class AIPlayerService implements PlayerService {
 
     private ItemUsageResponseDto useShotGun(Item shotGun, PlayerDataDto rival, GameStateDto gameStateDto) {
         int firstBulletDamage = gameStateDto.bullets().getFirstBulletDamage();
-        if((nextBullet != null && nextBullet.equals(BLUE)) || red < blue) {
+        // 다음 발사될 탄을 알고 있는 경우
+        if((nextBullet != null && nextBullet.equals(RED))) {
+            return shootAtRival(shotGun, rival, gameStateDto, firstBulletDamage);
+        }
+        if((nextBullet != null && nextBullet.equals(BLUE))) {
             return shootAtMe(shotGun, rival, gameStateDto, firstBulletDamage);   
-        } 
-        return shootAtRival(shotGun, rival, gameStateDto, firstBulletDamage);
+        }
+
+        // TODO: gameStateDto.bullets()를 통해 (실탄 / 공포탄) 수를 가져로도록 구현하기
+        // 다음 발사될 탄을 모르는 경우, 남아있는 (실탄 / 공포탄) 수에 따라 결정
+        // if(red >= blue) {
+        //     return shootAtRival(shotGun, rival, gameStateDto, firstBulletDamage);
+        // }
+        return shootAtMe(shotGun, rival, gameStateDto, firstBulletDamage);
     }
 
     private ItemUsageResponseDto shootAtMe(Item shotGun, PlayerDataDto rival, 
                                             GameStateDto gameStateDto, int firstBulletDamage) {
         outputView.println("딜러가 자신을 향해 총을 겨눕니다!");
+        Timer.delay(1000);
         printResultOfShot(firstBulletDamage);
         ItemUsageRequestDto targetingMe = shotGun.useItem(
                 new ItemUsageRequestDto(player.makePlayerDataDto(), 
@@ -114,6 +122,7 @@ public class AIPlayerService implements PlayerService {
     private ItemUsageResponseDto shootAtRival(Item shotGun, PlayerDataDto rival, 
                                                 GameStateDto gameStateDto, int firstBulletDamage) {
         outputView.println("딜러가 당신을 향해 총을 겨눕니다!");
+        Timer.delay(1000);
         printResultOfShot(firstBulletDamage);
         ItemUsageRequestDto targetingRival = shotGun.useItem(
                 new ItemUsageRequestDto(player.makePlayerDataDto(), rival, gameStateDto));
@@ -156,22 +165,11 @@ public class AIPlayerService implements PlayerService {
     }
 
     /**
-     * 딜러가 다음에 발사될 탄환이 무엇인지 확신합니다.
-     * @param nextBullet 다음에 발사될 탄환
-     */
-    private void decideNextBullet(Bullet nextBullet) {
-        if(nextBullet.equals(BulletConfig.RED)) {
-            this.nextBullet = BulletConfig.RED;
-        } else {
-            this.nextBullet = BulletConfig.BLUE;
-        }
-    }
-
-    /**
      * 딜러의 아이템 사용 메시지를 출력합니다.
      * @param item 사용된 아이템
      */
     private void printUsingItem(Item item) {
+        Timer.delay(1000);
         outputView.println("딜러가 " + item.toString() + "을(를) 사용합니다.\n");
         Timer.delay(1000);
     }
