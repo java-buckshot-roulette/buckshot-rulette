ecf2a54699dfcfeb56fb4ed37264c3f8cd340411
kkh0920 <rudgns1535@gmail.com>
2024-11-22 12:03:36 +0900

    fix: 돋보기 아이템 사용 시, 아이템이 소모되지 않는 현상

--- src/main/java/game/domain/item/MagnifyingGlass.java
+++ src/main/java/game/domain/item/MagnifyingGlass.java
@@ -7,7 +7,7 @@ import game.dto.ItemUsageRequestDto;
 public class MagnifyingGlass implements Item {
     @Override
     public ItemUsageRequestDto useItem(ItemUsageRequestDto itemUsageRequestDto) {
-        return itemUsageRequestDto;
+        return itemUsageRequestDto.reduceCasterItem(this);
     }
 
     @Override
