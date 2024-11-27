e4510e26660d4462d3de54007c6cf16bf6a843f6
kkh0920 <rudgns1535@gmail.com>
2024-11-27 10:50:52 +0900

    fix: 게임 결과 메세지 수정

--- src/main/java/game/service/stage/GameResult.java
+++ src/main/java/game/service/stage/GameResult.java
@@ -3,8 +3,8 @@ package game.service.stage;
 public enum GameResult {
     ONGOING("계속 진행 중"),
     GO_NEXT_STAGE("스테이지 클리어"),
-    GAME_OVER("당신은 죽었습니다.."),
-    GAME_CLEAR("축하합니다!");
+    GAME_OVER("당신은 사망하였습니다.."),
+    GAME_CLEAR("딜러가 사망하였습니다.\n\n축하합니다!");
 
     private final String stageMessage;
 
