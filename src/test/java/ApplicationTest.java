15ab3b0b0de8f200933c06d10b82b26c8cab26ca
HanJunSeoJB <hjs123@jbnu.ac.kr>
2024-11-27 19:07:48 +0900

    feat: ApplicationTest.java 테스트 코드 추가
    Application run 메소드에 대한 테스트 추가

--- src/test/java/ApplicationTest.java
+++ src/test/java/ApplicationTest.java
@@ -15,6 +15,7 @@ import org.mockito.ArgumentCaptor;
 class ApplicationTest {
 
     @Test
+    @DisplayName("1, 2가 아닌 다른 숫자를 넣었을 때 예외 처리")
     void shouldPrintErrorMessageWhenInputIs3() throws IOException {
         // Arrange
         OutputView mockOutputView = mock(OutputView.class); // OutputView를 Mock 객체로 생성
@@ -28,11 +29,10 @@ class ApplicationTest {
         when(mockInputView.askPersonToSelect())
                 .thenReturn("3"); // 3을 입력해 게임 시작 선택
 
-        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class); // 출력 메시지 캡처 생성
-
         // Act
         GameState result = app.selectMenu();
 
+        verify(mockOutputView, times(1)).println("잘못된 입력입니다. 다시 입력해주세요.\n"); // 출력 메시지 확인
         assertNull(result, "GameState가 null이 아닙니다.");
     }
 
@@ -51,9 +51,8 @@ class ApplicationTest {
         when(mockInputView.askPersonToSelect())
                 .thenReturn("invalid_input"); // 문자 입력 설정
 
-        // Act & Assert
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            app.selectMenu(); // selectMenu 호출 시 예외 발생 예상
-        });
+        GameState result = app.selectMenu();
+
+        verify(mockOutputView, times(1)).println("숫자를 입력해주세요"); // 출력 메시지 확인
     }
 }
